e3d2e1f0c8468e68ec59b1e6eaa577cae9d2869d
mingi <mingiyan.1999@mail.ru>
2025-01-10 22:16:23 +0300

    Новые обновленные запросы с Express и get-запрос с фильтрацией

--- src/routes/games.js
+++ src/routes/games.js
@@ -25,14 +25,68 @@ router.get('/',async(req,res)=>{
         /*console.error('Ошибка при выполнении запроса', err.stack)
         res.writeHead(500, { 'Access-Control-Allow-Origin': 'http://localhost:5173' })
         res.end(JSON.stringify({error:'Ошибка при выборке данных',details: err.message}))*/
-        console.error('Ошибка при выполнении запроса', err.stack)
+        console.error('Ошибка при выполнении запроса', error.stack)
         res.status(500).json({error:'Ошибка при выборке данных',details: err.message})
+    }
+})
+
+// GET-запрос с фильтрацией по жанру
+router.get('/:genres',async(req,res)=> {
+    const {genres} = req.params
+    try {
+        if(!genres){
+            return res.status(400).json({error: 'Необходимо указать жанры для фильтрации'})
+        }
+        // Преобразуем жанры из строки в массив
+        const genresArray = genres.split(',')
+
+        const query = 'SELECT * FROM games WHERE genres && $1'
+        const values = [genresArray]
+
+        const result = await dbClient.query(query,values)
+        
+    // Форматируем данные (например, преобразуем изображение)
+    const games = result.rows.map(game => ({
+        ...game,
+        game_img: game.game_img ? game.game_img.toString('base64') : null,
+    }));
+
+    res.status(200).json(games);
+} catch (error) {
+    console.error('Ошибка при выполнении запроса:', error.stack);
+    res.status(500).json({ error: 'Ошибка сервера', details: error.message });
+}
+})
+
+// POST - запрос на добавление игры
+router.post('/',async(req,res)=> {
+    const {name,price,genres,game_img} = req.body
+    try{
+        // Преобразование в данные типа base64 для отображения
+        const gameImgBuffer = Buffer.from(game_img,'base64')
+        const result = await dbClient.query('INSERT INTO games (name,price,genres,game_img) VALUES ($1,$2,$3,$4)',[name,price,genres,gameImgBuffer])
+        res.status(201).json(result.rows[0])
+    }catch(error) {
+        console.error('Ошибка добавления игры:',error)
+        res.status(500).json({error:'Ошибка сервера'})
+    }
+})
+
+// DELETE - запрос на удаление игры
+router.delete('/',async(req,res)=> {
+    const {id} = req.body
+    try{
+        await dbClient.query('DELETE FROM games WHERE id = $1',[id])
+        res.status(200).send('Игра успешно удалена')
+    } catch(error){
+        console.error('Ошибка при выполнении запроса', error.stack)
+        res.status(500).send('Ошибка удаления игры')
     }
 })
 
 export default router
 
-export const handleGames = async(req,res) => {
+/*export const handleGames = async(req,res) => {
     if(req.method==='GET') {
         try {
             const result = await dbClient.query('SELECT * FROM games');
@@ -88,29 +142,4 @@ export const handleGames = async(req,res) => {
         res.writeHead(404)
         res.end('Not Found')
     }
-
-    // Запрос на получение всех игр
-    async function getAllGames(){
-        try {
-            // Запрос в SQL
-            const result = await dbClient.query('SELECT * FROM games')
-            // Переменная для хранения игр
-            const games = result.rows.map(game => ({
-                ...game, 
-                game_img:game.game_img ? game.game_img.toString('base64'):null,
-            }))
-
-            res.writeHead(200, {
-                'Content-Type': 'application/json', // Тип данных - JSON
-                'Access-Control-Allow-Origin': 'http://localhost:5173', // Разрешить доступ только источнику с этого порта, иначе писать *
-            })
-            // Отправка данных в JSON
-            res.end(JSON.stringify(games))
-        } catch (error){
-            console.error('Ошибка при выполнении запроса', err.stack);
-            res.writeHead(500, { 'Access-Control-Allow-Origin': 'http://localhost:5173' });
-            res.end(JSON.stringify({error:'Ошибка при выборке данных',details: err.message}));
-        }
-        
-    }
-}
+}*/
--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 // Middleware для CORS
 app.use((req,res,next) =>{
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+  res.setHeader('Access-Control-Allow-Origin', '*')// http://localhost:5173
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   if(req.method==='OPTIONS'){
