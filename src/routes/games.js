e9246a1e9fa14af2a2e899c0bf51312c8712d216
mingi <mingiyan.1999@mail.ru>
2025-01-19 18:41:58 +0300

    GET-запрос для получения игры по id

--- src/routes/games.js
+++ src/routes/games.js
@@ -11,20 +11,10 @@ router.get('/',async(req,res)=>{
         const games = result.rows.map(game => ({
             ...game, 
             game_img:game.game_img ? game.game_img.toString('base64'):null,
-        }))
-
-        /*res.writeHead(200, {
-            'Content-Type': 'application/json', // Тип данных - JSON
-            'Access-Control-Allow-Origin': 'http://localhost:5173', // Разрешить доступ только источнику с этого порта, иначе писать *
-        })    
-        // Отправка данных в JSON
-        res.end(JSON.stringify(games))*/
+        }))   
         // Отправка данных в JSON
         res.status(200).json(games)
     } catch (error){
-        /*console.error('Ошибка при выполнении запроса', err.stack)
-        res.writeHead(500, { 'Access-Control-Allow-Origin': 'http://localhost:5173' })
-        res.end(JSON.stringify({error:'Ошибка при выборке данных',details: err.message}))*/
         console.error('Ошибка при выполнении запроса', error.stack)
         res.status(500).json({error:'Ошибка при выборке данных',details: err.message})
     }
@@ -57,11 +47,13 @@ router.get('/:genres',async(req,res)=> {
 
 // POST - запрос на добавление игры
 router.post('/',async(req,res)=> {
-    const {name,price,genres,game_img} = req.body
+    const {name,price,genres,game_img,description,rating,developer} = req.body
     try{
         // Преобразование в данные типа base64 для отображения
         const gameImgBuffer = Buffer.from(game_img,'base64')
-        const result = await dbClient.query('INSERT INTO games (name,price,genres,game_img) VALUES ($1,$2,$3,$4)',[name,price,genres,gameImgBuffer])
+        const result = await dbClient.
+        query('INSERT INTO games (name,price,genres,game_img,description,rating,developer) VALUES ($1,$2,$3,$4,$5,$6,$7)'
+            ,[name,price,genres,gameImgBuffer,description,rating,developer])
         res.status(201).json(result.rows[0])
     }catch(error) {
         console.error('Ошибка добавления игры:',error)
@@ -81,62 +73,28 @@ router.delete('/',async(req,res)=> {
     }
 })
 
-export default router
-
-/*export const handleGames = async(req,res) => {
-    if(req.method==='GET') {
-        try {
-            const result = await dbClient.query('SELECT * FROM games');
-            const games = result.rows.map(game => ({
-                ...game,
-                game_img: game.game_img.toString('base64'),
-            }));
-    
-            res.writeHead(200, {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': 'http://localhost:5173', // Разрешить доступ с любого источника
-            });
-            res.end(JSON.stringify(games));
-        } catch (err) {
-            console.error('Error executing query', err.stack);
-            res.writeHead(500, { 'Access-Control-Allow-Origin': 'http://localhost:5173' });
-            res.end('Error fetching data');
+// GET - запрос на получение игры по её id
+router.get('/gamePage/:id', async(req,res)=> {
+    const {id} = req.params
+    try {
+        const result = await dbClient.query('SELECT * FROM games WHERE id= $1',[id])
+        
+        // Проверка наличия игры
+        if(result.rows.length===0){
+            return res.status(404).json({error:`Игра с id=${id} не найдена`})
         }
-    } else if (req.method==='POST') {
-        let body =''
-        req.on('data', (chunk) => {body +=chunk.toString()})
-        req.on('end',async()=> {
-            try {
-                const {name,price,genres,game_img} = JSON.parse(body)
-                const gameImgBuffer = Buffer.from(game_img,'base64')
-                await dbClient.query('INSERT INTO games (name,price,genres,game_img) VALUES ($1,$2,$3,$4)',[name,price,genres,gameImgBuffer])
-                res.writeHead(201)
-                res.end('Game added successfuly')
-            } catch(err){
-                console.error('Error executing query',err.stack)
-                res.writeHead(500)
-                res.end('Error adding game')
-            }
-        })
-    } else if(req.method==='DELETE') {
-        let body =''
-        req.on('data',(chunk)=>{body+=chunk.toString()})
-        req.on('end',async()=>{
-            try {
-                const {id}=JSON.parse(body)
-                await dbClient.query('DELETE FROM games WHERE id = $1',[id])
-                res.writeHead(200)
-                res.end('Game delete successfuly')
-            } catch(err){
-                console.error('Error executing query',err.stack)
-                    res.writeHead(500)
-                    res.end('Error deleting game')
-            }
-        })
 
+        // Форматируем данные (например, преобразуем изображение)
+        const game = {
+            ...result.rows[0],
+            game_img: result.rows[0].game_img ? result.rows[0].game_img.toString('base64') : null,
+        };
+        
+        res.status(200).json(game)
+    } catch(error){
+        console.error('Ошибка поиска игры с id:',id)
+        res.status(500).json({error:'Ошибка сервера'})
     }
-    else {
-        res.writeHead(404)
-        res.end('Not Found')
-    }
-}*/
+})
+
+export default router
--- src/server.js
+++ src/server.js
@@ -31,17 +31,3 @@ const PORT = process.env.PORT||3000
 http.createServer(app).listen(PORT,() => {
   console.log(`Сервер запущен на http://localhost:${PORT}`)
 })
-
-/*const server = http.createServer((req, res) => {
-    if (req.url.startsWith('/games')) {
-      handleGames(req, res);
-    } else {
-      res.writeHead(404);
-      res.end('Not Found');
-    }
-  });
-  
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-})*/
