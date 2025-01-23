e97b45ba2104e537fff13b0f78a20eb8d23d9da3
mingi <mingiyan.1999@mail.ru>
2025-01-23 13:05:16 +0300

    post-запрос авторизации

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,11 +3,31 @@ import express from  'express'
 const router = express.Router()
 
 // POST - Запрос для авторизации пользователя
-router.post('/auth-user',async(req,res)=>{
+router.post('/',async(req,res)=>{
+    const {login,password} = req.body
     try{
+        const data = await dbClient.query(
+            'SELECT * FROM users_credential WHERE login=$1',
+            [login]
+        )
 
+        if(data.rows.length ===0){
+            return res.status(404).json(`Неправильный логин`)
+        }
+
+        if(data.rows[0].password!=password){
+            return res.status(400).json(`Неправильный пароль`)
+        }
+
+        const result = await dbClient.query(
+            'SELECT * FROM users_info WHERE user_info_id = $1',
+            [data.rows[0].user_info_id]
+        )
+
+        res.status(200).json(result.rows)
     } catch(error) {
-
+        console.error('Ошибка при выполнении запроса:', error.stack);
+        res.status(500).json({ error: 'Ошибка сервера', details: error.message });
     }
 })
 
