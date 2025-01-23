84534729348cd945acd558f13f126d6c4dd37655
mingi <mingiyan.1999@mail.ru>
2025-01-23 12:46:28 +0300

    get запросы на получение юзеров по id

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,16 +2,6 @@ import dbClient from "../db/client.js"
 import express from  'express'
 const router = express.Router()
 
-// GET - Запрос для получения учетных данных и 
-// личной информации всех пользователей
-router.get('/users',async(req,res)=>{
-    try{
-
-    } catch(error){
-
-    }
-})
-
 // POST - Запрос для авторизации пользователя
 router.post('/auth-user',async(req,res)=>{
     try{
--- src/routes/users.js
+++ src/routes/users.js
@@ -18,6 +18,27 @@ router.get('/info',async(req,res)=>{
         res.status(500).json({error:"Ошибка сервера:",details:error})
     }
 })
+
+// GET -Запрос на получение ЛИ юзера по id
+router.get('/info/:id',async(req,res)=>{
+    const {id} = req.params
+    try{
+        const result = await dbClient.query(
+            'SELECT * FROM users_info WHERE user_info_id=$1',[id]
+        )
+
+        if(result.rows.length===0){
+                return res.status(404).json(`Пользователь с id ${id} не найден`)
+            }
+
+        res.status(200).json(result.rows)
+    } catch(error){
+        console.error('Ошибка при выполнении запросы ',error.stack)
+        res.status(500).json({error:"Ошибка сервера:",details:error})
+    }
+})
+
+
 // GET - Запрос на получение личной информации пользователей
 router.get('/credential',async(req,res)=>{
     try{
@@ -29,6 +50,27 @@ router.get('/credential',async(req,res)=>{
         res.status(200).json(result.rows)
     } catch(error){
         console.error('Ошибка при выполнении запроса',error.stack)
+        res.status(500).json({error:"Ошибка сервера:",details:error})
+    }
+})
+
+
+// GET - Запрос на получение УД юзера по id
+router.get('/credential/:id',async(req,res)=>{
+    const {id} = req.params
+    try{
+        const result = await dbClient.query(
+            'SELECT * FROM users_credential WHERE user_credential_id = $1',
+            [id]
+        )
+
+        if(result.rows.length===0){
+            return res.status(404).json(`Пользователь с id ${id} не найден`)
+        }
+
+        res.status(200).json(result.rows)
+    }catch(error){
+        console.error('Ошибка при выполнении запроса ', error.stack)
         res.status(500).json({error:"Ошибка сервера:",details:error})
     }
 })
